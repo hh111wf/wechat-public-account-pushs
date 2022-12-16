ad5d7de95d97aca2a53848a59c4bf5971a82394c
hh111wf <93304689+hh111wf@users.noreply.github.com>
2022-12-16 19:47:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNpOa5lidBuqW2pxg7MJiomQ0ODc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pbcoNaikNEaYqIatRsT-TZumYnZawpzJzGJut3URGHs',
+      useTemplateId: 'qxm-VtL3iSbAK_WlX9aZkdZWxrEvCCCP_mcnEkT1gj8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-17',
       festivals: [
