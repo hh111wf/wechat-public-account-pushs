2a1dbfe75b834b7d650ae9ab7bfff1c77f15d8a1
hh111wf <93304689+hh111wf@users.noreply.github.com>
2022-12-16 19:31:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '哈喽',
+      name: '哈喽，上午好啊！',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNpOa5lidBuqW2pxg7MJiomQ0ODc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,14 +29,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '你的', year: '2002', date: '09-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '我的的', year: '2002', date: '07-19',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2019', date: '08-31',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
